2d253d9b726bd273f0d3c5ca3f9c66b794959c37
Yash Bhawsar <yashbhawsar97@gmail.com>
2025-01-08 20:23:30 +0530

    setup router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/navbar";
+import Home from "./components/Home";
+import Paste from "./components/Paste";
+import ViewPaste from "./components/ViewPaste";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <Navbar />
+        <Home />
+      </div>
+    ),
+  },
+  {
+    path: "/pastes",
+    element: (
+      <div>
+        <Navbar />
+        <Paste />
+      </div>
+    ),
+  },
+  {
+    path: "/pastes/:id",
+    element: (
+      <div>
+        <Navbar />
+        <ViewPaste />
+      </div>
+    ),
+  },
+]);
 
 function App() {
   return (
     <>
-      <h3>Paste.in</h3>
+      <RouterProvider router={router} />
     </>
   );
 }
